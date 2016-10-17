73346b971a9c3c5303eebd4cc0b96b6fd523dd1a
AN <thuyannguyen306@gmail.com>
2016-10-18 05:17:50 +0900

    first commit

---
+++ PracticeTestGit/PracticeTestGit/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PracticeTestGit
+//
+//  Created by Apple on 10/18/16.
+//  Copyright (c) 2016 NA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.redColor()
+        self.view.backgroundColor = UIColor.blueColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
